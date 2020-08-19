0479011d2e02272b08e1ad8e4964fd704c54442f
amana1221 <62594393+amana1221@users.noreply.github.com>
2020-08-19 14:43:04 +0900

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,12 +8,17 @@ class TestController extends Controller
 {
     public function index()
     {
-     $name = "abe";
+     //name = "abe";
      
      // return view('test',compact('name'));
      
-     $tell = "090";
+     //$tell = "090";
      
-     return view('test')->with('denwa',"080");
+     //return view('test')->with('denwa',"080");
+        
+    $test_array = ["red","green", "blue"];
+    
+    return view('test.normal',compact('test_array'));
+        
     }
 }
