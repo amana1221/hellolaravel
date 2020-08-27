6ddfcd7804c7773b62bc68746d7810603337f170
amana1221 <free.spirit.ma1221@gmail.com>
2020-08-27 09:59:14 +0900

    Revert "add ECphp"
    This reverts commit 2837626d1bc5dda88b18c01c9a7ea09bc39d2b9b.

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,10 +8,6 @@ class HelloController extends Controller
 {
     public function index()
     {
-      //return view('hello');
-        
-      $test_1 = "テスト";
-
-     return view('Hello')->with('test_1',$test_1);
+      return view('hello');
     }
 }
